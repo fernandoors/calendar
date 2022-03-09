7f3480772abdfe1ca033f62cf916cff2cc2588d5
fernandoors <fernando.ors@outlook.com>
2022-03-09 01:30:31 -0300

    feat add reminder list for group by days

---
+++ src/components/reminder-list/reminders-list.jsx
@@ -0,0 +1,67 @@
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { reminderActions } from '../../store/reminder/reminder.store';
+import { groupRemindersByDay } from '../../store/reminder/reminder.utils';
+
+import { formatedDate } from '../../utils/date';
+
+import RemindersItem from '../reminder-item/reminder-item';
+
+import styles from './reminders-list.module.css';
+
+function RemindersList() {
+  const dispatch = useDispatch();
+  const { reminders, filterByDay } = useSelector((store) => store.reminder);
+
+  const remindersByDay = useMemo(
+    () =>
+      groupRemindersByDay(reminders).filter(
+        ({ day }) => !filterByDay || formatedDate(day) === filterByDay,
+      ),
+    [reminders, filterByDay],
+  );
+
+  function handleCloseFilter() {
+    dispatch(reminderActions.selectFilterDay(null));
+  }
+
+  function handleAddReminder() {
+    dispatch(reminderActions.add());
+  }
+
+  return (
+    <div className={styles.container}>
+      <h4>
+        Next Reminders:
+        {!!filterByDay ? (
+          <>
+            <label>{filterByDay}</label>
+            <span
+              className="fa fa-times-circle"
+              onClick={handleCloseFilter}
+            ></span>
+          </>
+        ) : (
+          <button className={styles.primary} onClick={handleAddReminder}>
+            Add
+          </button>
+        )}
+      </h4>
+      {!remindersByDay.length ? (
+        <h4>Reminders not found</h4>
+      ) : (
+        remindersByDay.map((reminderContent) => (
+          <div key={reminderContent.day}>
+            {!filterByDay ? <p> {reminderContent.day} </p> : null}
+            {reminderContent.reminders.map((reminder) => (
+              <RemindersItem reminder={reminder} key={reminder.id} />
+            ))}
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default RemindersList;
