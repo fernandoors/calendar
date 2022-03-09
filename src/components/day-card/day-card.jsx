0a69529e73d4b988f566e93edd117561ebaed744
fernandoors <fernando.ors@outlook.com>
2022-03-09 01:27:21 -0300

    feat add day card for calendar components

---
+++ src/components/day-card/day-card.jsx
@@ -0,0 +1,64 @@
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { reminderActions } from '../../store/reminder/reminder.store';
+import { converDateToString, formatedDate } from '../../utils/date';
+
+import RemindersItem from '../reminder-item/reminder-item';
+
+import styles from './day-card.module.css';
+
+export default function DayCard({ day, date }) {
+  const dispatch = useDispatch();
+  const { filterByDay, reminders, reminderSelected } = useSelector(
+    (store) => store.reminder,
+  );
+
+  function handleSelectFilterDay(dateSelected) {
+    dispatch(reminderActions.selectFilterDay(dateSelected));
+  }
+
+  function handleAddReminderWithDate() {
+    const time = new Date().toTimeString().split(' ')[0];
+    const dateAdded = `${date} ${time}`;
+    dispatch(reminderActions.add({ date: converDateToString(dateAdded) }));
+  }
+
+  const remindersFiltersByDay = useMemo(
+    () =>
+      reminders.filter(
+        (remind) => formatedDate(remind.date) === formatedDate(date),
+      ),
+    [reminders, date],
+  );
+
+  return (
+    <div
+      onClick={() => handleSelectFilterDay(date)}
+      data-testid="day-card"
+      className={styles.container}
+      style={{
+        background: filterByDay === date ? '#6495ed' : '#f5f5dc42',
+      }}
+    >
+      <h4 className={styles.day}>
+        {day}
+        <span
+          onClick={handleAddReminderWithDate}
+          className={styles.add + ' fa fa-plus'}
+        />
+      </h4>
+      <div className={styles.list}>
+        {remindersFiltersByDay.map((reminder) => (
+          <RemindersItem
+            reminder={reminder}
+            isSelected={
+              !!reminderSelected && reminder.id === reminderSelected.id
+            }
+            key={reminder.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
